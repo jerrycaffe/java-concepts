8ee9c0b27f02d1997354b1ba21cf277199b0e669
jerrycaffe@gmail.com <jerrycaffe@gmail.com>
2022-09-19 12:24:53 +0100

    worked on inheritance in java

---
+++ src/JavaConceptNine.java
@@ -0,0 +1,31 @@
+//The order of accessibility in Java remains private, default, protected and public
+//To overwrite methods in java this should be obeyed i.e the order...
+//e.g if a parent class declares a method as protected, overwriting class should use public
+
+
+class ParentNine{
+    public void fun(){
+        System.out.println("Inside Parent Class");
+    }
+    protected void pFun(){
+        System.out.println("Private method within parent");
+    }
+}
+class ChildNine extends ParentNine{
+//Overloading fun function
+    public void fun(String x){
+        System.out.println("Inside Child class nine "+x);
+    }
+    public void pFun(){
+        System.out.println("Private method within child");
+    }
+}
+
+public class JavaConceptNine {
+    public static void main(String[] args) {
+ChildNine c = new ChildNine();
+c.fun();
+c.fun("Jerry");
+c.pFun();
+    }
+}
