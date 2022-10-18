7bf836ebd662ffeee61f288233c026955c27635e
jerrycaffe <jerrycaffe@gmail.com>
2022-10-18 05:56:52 +0100

    java concept thirty two

---
+++ src/JavaConceptThirtyTwo.java
@@ -0,0 +1,21 @@
+//Try and catch cannot be inside class directly
+class JavaThirtyTwo {
+//try{}catch(Exception e) This line of code throws error as it is directly inside the class
+    JavaThirtyTwo(int a){
+        try{
+            var sum = 2/0;
+            System.out.println(sum);
+        }catch(Exception e){
+            System.out.println("The error is that we are "+e.getMessage());
+        }
+
+    }
+}
+
+public class JavaConceptThirtyTwo {
+    public static void main(String[] args) {
+
+        System.out.println(new JavaThirtyTwo(20));
+    }
+
+}
