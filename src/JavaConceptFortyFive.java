cd94299e612dd75d63ef344ab28427461e4c263f
jerrycaffe <jerrycaffe@gmail.com>
2022-11-02 10:47:31 +0100

    worked on java concept forty five

---
+++ src/JavaConceptFortyFive.java
@@ -0,0 +1,15 @@
+//Interface
+//can have only public modifiers
+//if no access modifier methods and variables are considered public
+
+//class
+//Can have public, default, protected and private
+//if no access modifier is specified methods and variables are considered default
+
+interface JavaFortyFive{
+    int num = 0; //this by default will be considered public
+    public int number = 20; //take note of the color of the keyword public, shows that is not necessary
+
+}
+public class JavaConceptFortyFive {
+}
