3cc02a5ca6351d2d26d841d12f901afb9be21df7
jerrycaffe <jerrycaffe@gmail.com>
2022-11-08 21:36:13 +0100

    talks about not accepting null values by primitive data

--- src/JavaConceptFortyEight.java
+++ src/JavaConceptFortyEight.java
@@ -4,6 +4,6 @@ public class JavaConceptFortyEight {
     public static void main(String[] args) {
         Object newObj = null; // do not throw error because Object is a reference variable
         String newStr = null; //do not throw error because String is a reference variable
-        int number = null; //Throws error because int is a primitive data type
+//        int number = null; //Throws error because int is a primitive data type
     }
 }
