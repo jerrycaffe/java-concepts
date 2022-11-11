fa96ce0b71edd8cdbffa04bb820d112a096937b7
jerrycaffe <jerrycaffe@gmail.com>
2022-11-11 04:04:39 +0100

    java concept fifty two is about not able to inherit from parent class marked as final

---
+++ src/JavaConceptFiftyTwo.java
@@ -0,0 +1,24 @@
+//final class cannot be inherited in ava
+final class JavaFiftyTwoShape{
+    int length;
+    int breadth;
+    JavaFiftyTwoShape(int length, int breadth){
+        this.breadth = breadth;
+        this.length = length;
+    };
+}
+
+class JavaFiftyTwoRectangle extends JavaFiftyTwoShape{
+    JavaFiftyTwoRectangle(int length, int breadth){
+        super(length, breadth);
+
+    }
+    void area(){
+        System.out.println("The area of a rectangle is "length*breadth);
+    }
+}
+public class JavaConceptFiftyTwo {
+    public static void main(String[] args) {
+
+    }
+}
